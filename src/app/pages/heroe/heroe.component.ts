8ccd09876d4343106d3c91322d8208f366f0f558
Alex Ballera <alexballera@gmail.com>
2020-04-21 08:57:21 -0300

    Creado PUT

--- src/app/pages/heroe/heroe.component.ts
+++ src/app/pages/heroe/heroe.component.ts
@@ -24,11 +24,19 @@ export class HeroeComponent implements OnInit {
       return;
     }
 
-    this.heroesService.crearHeroe(this.heroe)
-    .subscribe(resp => {
-      console.log(resp);
-      this.heroe = resp;
-    });
+    if (this.heroe.id) {
+      this.heroesService.actualizarHeroe(this.heroe)
+      .subscribe(resp => {
+        console.log(resp);
+      });
+    } else {
+      this.heroesService.crearHeroe(this.heroe)
+      .subscribe(resp => {
+        console.log(resp);
+        this.heroe = resp;
+      });
+    }
+
   }
 
 }
